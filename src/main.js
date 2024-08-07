e064c0b70460479f5a23d1da8f6c492788b8373b
CLCK0622 <zhongyi070622@163.com>
2024-08-07 14:08:59 +0800

    solve localStorage bug

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-function change_mode() {
-    if (localStorage.theme === 'light' || !('theme' in  localStorage)) {
-        localStorage.theme = 'dark'
-    } else {
-        localStorage.theme = 'light'
+function init_mode() {
+    if ('theme' in localStorage) {
+        if (localStorage.theme === 'dark') {
+            document.getElementById("darkmode").setAttribute("on", true)
+            document.getElementById("darkmode").checked = true
+        }
     }
     if (localStorage.theme === 'light' || (!('theme' in  localStorage))) {
         document.getElementById("html").classList.remove('dark')
@@ -11,8 +12,17 @@ function change_mode() {
     }
 }
 
-  // Whenever the user explicitly chooses light mode
-//   localStorage.theme = 'light'
-  
-  // Whenever the user explicitly chooses dark mode
-//   localStorage.theme = 'dark'
+function change_mode() {
+    if (localStorage.theme === 'light' || !('theme' in  localStorage)) {
+        localStorage.theme = 'dark'
+        document.getElementById("darkmode").setAttribute("on", true)
+    } else {
+        localStorage.theme = 'light'
+        document.getElementById("darkmode").setAttribute("on", false)
+    }
+    if (localStorage.theme === 'light' || (!('theme' in  localStorage))) {
+        document.getElementById("html").classList.remove('dark')
+    } else {
+        document.getElementById("html").classList.add('dark')
+    }
+}
