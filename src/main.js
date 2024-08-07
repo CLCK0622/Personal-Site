c1fe74a5ecb9c2a911cd778e0a87beb051de6d96
CLCK0622 <zhongyi070622@163.com>
2024-08-07 12:51:57 +0800

    dark mode toggle

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
+function change_mode() {
+    if (localStorage.theme === 'light' || !('theme' in  localStorage)) {
+        localStorage.theme = 'dark'
+    } else {
+        localStorage.theme = 'light'
+    }
+    if (localStorage.theme === 'light' || (!('theme' in  localStorage))) {
+        document.getElementById("html").classList.remove('dark')
+    } else {
+        document.getElementById("html").classList.add('dark')
+    }
+}
 
-  
   // Whenever the user explicitly chooses light mode
 //   localStorage.theme = 'light'
   
   // Whenever the user explicitly chooses dark mode
 //   localStorage.theme = 'dark'
-  
-  // Whenever the user explicitly chooses to respect the OS preference
-//   localStorage.removeItem('theme')
